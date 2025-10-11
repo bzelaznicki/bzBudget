4d6b889caecabe367c3503cc2546ccfa633c771c
bzelaznicki <bartosz@zelaznicki.com>
2025-10-12 00:31:19 +0200

    better auth init

---
+++ src/lib/auth.ts
@@ -0,0 +1,15 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db/db";
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+	}),
+	emailAndPassword: {
+		enabled: true,
+		async sendResetPassword(data, request) {
+			console.log("Sent reset password");
+		},
+	},
+});
