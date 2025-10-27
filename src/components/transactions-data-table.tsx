bab74a22184afe08f014605ab98f0ffe6bf4c266
bzelaznicki <bartosz@zelaznicki.com>
2025-10-28 00:18:09 +0100

    api shapes in transaction table

--- src/components/transactions-data-table.tsx
+++ src/components/transactions-data-table.tsx
@@ -2,18 +2,18 @@
 import { useState } from "react";
 import { TransactionResponse } from "@/db/queries/transactions";
 
-
 export async function TransactionsDataTable() {
 
 	const [error, setError] = useState<string | null>(null);
 	const [transactions, setTransactions] = useState<TransactionResponse[]>([]);
+	const [totalTransactions, setTotalTransactions] = useState<number>(0);
 	const [currentPage, setCurrentPage] = useState<number>(1);
 	const [totalPages, setTotalPages] = useState<number>(0);
 	const [accounts, setAccounts] = useState<Record<string, string> | null>(null);
 	const [perPage, setPerPage] = useState<number>(10);
 
-	const fetchTransactions = async () => {
-		const url = "/api/transactions";
+	const fetchTransactions = async (page: number, perPage: number) => {
+		const url = `/api/transactions?page=${page}&perPage=${perPage}`;
 
 		try {
 			const res = await fetch(url);
@@ -24,12 +24,20 @@ export async function TransactionsDataTable() {
 				return;
 			}
 
+			const body: { total: number, pages: number, transactions: TransactionResponse[] } = await res.json();
+
+
+			setTransactions(body.transactions);
+			setTotalPages(body.pages);
+			setTotalTransactions(body.total);
+
+
 		} catch (err: unknown) {
 			if (err instanceof Error) {
 				setError(err.message);
 				return;
 			}
-			setError("Error fetching transactions");§
+			setError("Error fetching transactions");
 		}
 	}
 
