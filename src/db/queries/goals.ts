067aacb6800b04d5003065a787bf8e08b941ac98
bzelaznicki <bartosz@zelaznicki.com>
2025-11-12 22:22:14 +0100

    add goal query

---
+++ src/db/queries/goals.ts
@@ -0,0 +1,25 @@
+import { db } from "../db";
+import { goals } from "../schema";
+
+
+const toDateString = (date?: Date) => date?.toISOString().slice(0, 10);
+
+export async function createGoal(usersId: string, name: string, targetAmount: number, goalType: "saving" | "spending", period: "one-time" | "monthly" | "weekly" | "yearly", startDate?: Date, endDate?: Date, categoriesId?: string, description?: string) {
+
+	const goalValues = {
+		usersId,
+		name,
+		targetAmount: targetAmount.toString(),
+		goalType,
+		period,
+		...(startDate && { startDate: toDateString(startDate) }),
+		...(endDate && { endDate: toDateString(endDate) }),
+		...(categoriesId && { categoriesId }),
+		...(description && { description }),
+	} satisfies typeof goals.$inferInsert;
+
+	const resp = await db.insert(goals).values(goalValues).returning();
+
+	return resp[0] ?? null;
+
+}
