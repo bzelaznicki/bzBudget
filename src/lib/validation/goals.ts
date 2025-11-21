b66a50c2b0c0a14c297f36c8632232bb56b98cd9
bzelaznicki <bartosz@zelaznicki.com>
2025-11-21 17:23:33 +0100

    added more validation

--- src/lib/validation/goals.ts
+++ src/lib/validation/goals.ts
@@ -20,4 +20,6 @@ export const goalsFormSchema = z.object({
 
 });
 
-export type GoalsFormTypes = z.infer<typeof goalsFormSchema>; 
+export type GoalsFormTypes = z.infer<typeof goalsFormSchema>;
+
+export const goalPayloadSchema = z.object({})
